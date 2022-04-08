0de072476b7d7617fc191cdc05f2a57921684144
Stephen Giordano <steve1nyc@gmail.com>
2022-04-08 18:01:39 -0400

    double quotes

--- services/requests.js
+++ services/requests.js
@@ -9,8 +9,8 @@ const pool = new Pool({ // create connection to database
 });
 
 const getAllActivities = (req, res) => {
-  const getString = 'SELECT * FROM my_activities'; // select all rows from the 'my_activities' table
-  const countString = 'SELECT count(*) FROM my_activities' // get total row count from the 'my_activities' table
+  const getString = 'SELECT * FROM "my_activities"'; // select all rows from the 'my_activities' table
+  const countString = 'SELECT count(*) FROM "my_activities"' // get total row count from the 'my_activities' table
   pool.query(getString) // send query to select all rows from the 'my_activities' table 
     .then(activityResults => {
       let activities = activityResults.rows;
@@ -37,7 +37,7 @@ const getSingleActivity = (req, res) => {
 const addActivityToDB = (req, res) => {
   const activity = [ req.body.activity ]
 
-  const addString = 'INSERT INTO my_activities (activity) VALUES ($1) RETURNING *'; // insert value into my_activities' table
+  const addString = 'INSERT INTO "my_activities" (activity) VALUES ($1) RETURNING *'; // insert value into my_activities' table
 
   pool.query(addString, activity)
     .then(result => res.json(result))
@@ -45,7 +45,7 @@ const addActivityToDB = (req, res) => {
 }
 
 const deleteAllActivites = (req, res) => {
-  const removeString = 'DELETE FROM my_activities'; // delete all items in the 'my_activities' table
+  const removeString = 'DELETE FROM "my_activities"'; // delete all items in the 'my_activities' table
   pool.query(removeString) // send query delete all items in the 'my_activities' table
     .then(res.send('All activities cleared!')) // send confirmation to the browser
     .catch(err => console.log(err));  
