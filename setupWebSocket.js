7c261c747fdb4874015faec4687148253314b62a
Stephen Giordano <steve1nyc@gmail.com>
2022-04-08 23:04:29 -0400

    websockets

--- setupWebSocket.js
+++ setupWebSocket.js
@@ -1,28 +1,29 @@
 // setupWebSocket.js
-const WebSocket = require("ws");
+const Server = require("ws");
 
 // accepts an http server (covered later)
 const setupWebSocket = (server) => {
   console.log('~~~ SET UP WEBSOKET');
   // ws instance
-  const wss = new WebSocket.Server({ noServer: true });
+  // const wss = new WebSocket.Server({ noServer: true });
 
+  const wss = new Server({ server });
   // // handle upgrade of the request
-  server.on("upgrade", function upgrade(request, socket, head) {
-    try {
-       // authentication and some other steps will come here
-       // we can choose whether to upgrade or not
+  // server.on("upgrade", function upgrade(request, socket, head) {
+  //   try {
+  //      // authentication and some other steps will come here
+  //      // we can choose whether to upgrade or not
 
-       wss.handleUpgrade(request, socket, head, function done(ws) {
-        wss.emit("connection", ws, request);
-       });
-    } catch (err) {
-      console.log("upgrade exception", err);
-      socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
-      socket.destroy();
-      return;
-    }
-  });
+  //      wss.handleUpgrade(request, socket, head, function done(ws) {
+  //       wss.emit("connection", ws, request);
+  //      });
+  //   } catch (err) {
+  //     console.log("upgrade exception", err);
+  //     socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+  //     socket.destroy();
+  //     return;
+  //   }
+  // });
 
   // what to do after a connection is established
   wss.on("connection", (ctx) => {
